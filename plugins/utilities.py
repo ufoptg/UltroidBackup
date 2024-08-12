52ad2db01832002614198e09180c4a637fa47593
TrueSaiyan <ufperth@protonmail.com>
2024-08-13 02:35:29 +0800

    Innocent Timezone

--- plugins/utilities.py
+++ plugins/utilities.py
@@ -141,7 +141,7 @@ collection = db["user_data"]
 
 # Define the UTC timezone and Perth timezone
 utc_tz = pytz.utc
-perth_tz = pytz.timezone("Australila/Perth")
+perth_tz = pytz.timezone("India/Mumbai")
 
 
 async def mention_user(user_id):
