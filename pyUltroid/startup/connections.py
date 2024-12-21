954ac46268859f0f7c48e0ae47037b1a0493cb68
TrueSaiyan <ufperth@protonmail.com>
2024-12-22 05:51:57 +0800

    Update connections.py

--- pyUltroid/startup/connections.py
+++ pyUltroid/startup/connections.py
@@ -81,8 +81,8 @@ def validate_session(session, logger=LOGS, _exit=True):
 
 async def vc_connection(udB, ultroid_bot):
     from strings import get_string
-
     VC_SESSION = Var.VC_SESSION or udB.get_key("VC_SESSION")
+
     if VC_SESSION and VC_SESSION != Var.SESSION:
         LOGS.info("Starting up VcClient.")
         try:
