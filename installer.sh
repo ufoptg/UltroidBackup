5f2d9ef9071585d505e95072452a6fa19007a30f
TrueSaiyan <ufperth@protonmail.com>
2025-01-06 14:48:08 +0800

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-REPO="https://github.com/TeamUltroid/Ultroid.git"
+REPO="https://github.com/ufoptg/UltroidBackup.git"
 CURRENT_DIR="$(pwd)"
 ENV_FILE_PATH=".env"
 DIR="/root/TeamUltroid"
